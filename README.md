2a6dbb2f243a37bb2a86791273b1ba31960e3f4e
Kadesa-Star <kadesaondego@yahoo.com>
2024-10-20 12:50:54 +0300

    start a script

---
+++ README.md
@@ -0,0 +1,10 @@
+# Markdown to HTML Converter
+
+## Description
+This project converts Markdown text to HTML. It supports basic Markdown features like headers, bold text, and lists.
+
+## Usage
+To run the converter, use:
+
+```bash
+./markdown_parser.py input.md output.html
---
+++ markdown2html.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+File takes two arguments
+script converts markdown into html file
+"""
+import sys
+import os
+
+
+# check the number of arguments supplied
+if len(sys.argv) < 3:
+    sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+    sys.exit(1)
+
+# get input and output files from supplied arguments
+markdown_file = sys.argv[1]
+html_file = sys.argv[2]
+
+# check if markdown file exists
+if not os.path.isfile(markdown_file):
+    sys.stderr.write(f"Missing {markdown_file}\n")
+    sys.exit(1)
+
+# if all is fine exit with 0 (success)
+sys.exit(0)
